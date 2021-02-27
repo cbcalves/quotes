4b41092a41717b7a2cb7bf75c3342828834bd39a
Bernardo Campos <62954018+bernardocamps@users.noreply.github.com>
2021-02-27 13:23:23 -0300

    mesclando melhorias realizadas no quotes-heroku-2
    o histórico de ajustes encontra-se no repositório quotes-heroku-2

---
+++ client/quotes.js
@@ -0,0 +1,44 @@
+let globalText = null;
+let globalAuthor = null;
+let globalButton = null;
+
+/**
+ * Mudar esta página para o local da API
+ */
+const globalPAGE = window.location.href + 'api/quotes';
+
+window.addEventListener('load', start);
+
+async function start() {
+    globalText = document.getElementById('text');
+    globalAuthor = document.getElementById('author');
+    globalButton = document.getElementById('newQuote');
+
+    globalButton.addEventListener('click', inspiracao);
+
+    inspiracao();
+}
+
+async function getQuote() {
+    try {
+        const res = await fetch(globalPAGE);
+        const quote = await res.json();
+        return quote;
+    } catch (err) {
+        console.log(err);
+        return null;
+    }
+}
+
+async function inspiracao() {
+    const quote = await getQuote();
+    if (quote) {
+        let { text, author } = quote;
+        if (!author) {
+            author = 'Unknown';
+        }
+
+        globalText.innerHTML = text;
+        globalAuthor.innerHTML = author;
+    }
+}

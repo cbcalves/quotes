5ce07607c9fba5d9e6eb6c93daa5e8a61b0619d7
Gabriel Mendonça <gabriel.mendonca@agenciamop.com.br>
2021-04-08 01:05:17 -0300

    Criar modelo para Quote

--- backend/src/model/quote.js
+++ backend/src/model/quote.js
@@ -1,10 +1,10 @@
-class Quote {
 
-  constructor (text, author) {
+class Quote {
+  constructor (id, text, author) {
+    this.id = id;
     this.text = text;
     this.author = author;
   }
-
-};
+}
 
 module.exports = Quote;

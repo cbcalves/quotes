ec7aa6f0174c733c81a19ca756c650bba0eaa6b4
Gabriel Mendonça <gabriel.mendonca@agenciamop.com.br>
2021-04-08 01:04:02 -0300

    Adicionar configurações de conexão do typeorm

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 // Importando Express, CORS e MongoDB
+const typeorm = require("typeorm");
 const express = require('express');
 const cors = require('cors');
 const path = require("path");
@@ -13,14 +14,33 @@ app.use(express.json());
 // Saiba mais: https://github.com/motdotla/dotenv
 require('dotenv').config();
 
-app.use(require('./routes'));
+app.use('/api/v1', require('./routes'));
 
 // Heroku utiliza uma variável de ambiente chamada process.env.PORT para armazenar a porta
 var port = process.env.PORT || 8080;
 
 app.listen(port, async() => {
+
     try {
-        console.log('Servidor ligado >> ' + port);
+        typeorm.createConnection({
+            type: process.env.DB_TYPE || 'mysql',
+            host: process.env.DB_HOST || 'localhost',
+            port: process.env.DB_PORT || 3304,
+            username: process.env.DB_USER || 'admin',
+            password: process.env.DB_PASS || '123456',
+            database: process.env.DB_NAME || 'quote',
+            synchronize: true,
+            logging: false,
+            entities: [
+                require("./entity/quote")
+            ]
+        }).then(function () {
+            console.log("Database connected");
+        }).catch(err => {
+            console.log(err.message);
+        });
+
+        console.log('Servidor ligado | ' + port);
     } catch (err) {
         console.log(err);
         process.exit(1);

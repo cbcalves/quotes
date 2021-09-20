766092685ce0d1dc830d82ff3f0b0aa9b3de5f29
Marcelo Pinheiro <mpinheir@gmail.com>
2021-09-19 19:17:00 -0700

    Improved comments and function titles.

--- quotes.js
+++ quotes.js
@@ -6,7 +6,7 @@ let globalAuthor = null;
 let globalQuotes = [];
 let globalButton = null;
 
-// Acionar quando a página estiver carregada
+// Aciona quando a página estiver carregada
 window.addEventListener('load', start);
 
 // Função assíncrona - conecta o HTML ao JS
@@ -18,9 +18,11 @@ async function start() {
     // Botão para solicitar uma nova Quote à API
     globalButton.addEventListener('click', start);
 
+    //Gets author and new quote from API
     await getQuotes();
 
-    inspiracao();
+    //Updates page with data received via API call
+    updatePage();
 }
 
 // Função assíncrona - recebe os dados da API
@@ -47,10 +49,10 @@ async function getQuotes() {
 }
 
 // Função que acessa os dados retornados pela API - JSON
-function inspiracao() {
+function updatePage() {
     if (globalQuotes.length === 0) {
         return;
     }
     globalText.innerHTML = globalQuotes.Quote;
     globalAuthor.innerHTML = globalQuotes.Author;
-} 
+}
